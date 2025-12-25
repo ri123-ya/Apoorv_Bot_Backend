fa56fcf3ecb6dea33fab37b0b9ec24c3f32789f1
Riya Rastogi <riya02rastogi@gmail.com>
2025-12-25 22:48:44 +0530

    updated

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
+  next();
+});
 
 app.use(express.json());
 app.use("/api", chatRoute);
