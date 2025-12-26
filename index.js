80d238697a83be0ae08b660f723cc85c93f3b2fc
Riya Rastogi <riya02rastogi@gmail.com>
2025-12-26 18:25:42 +0530

    updated

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 dotenv.config();
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL, 
+  origin: "https://apoorvbot.netlify.app", 
   // credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization", "Accept"], 
